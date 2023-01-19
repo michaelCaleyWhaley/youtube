c3f53138af2efe76abc74e142c9916431eea63f6
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2023-01-19 18:37:19 +0000

    working webpack dev server

---
+++ caleycode-todo/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    port: 9000,
+    client: {
+      progress: true,
+    },
+    hot: false,
+  },
+};
